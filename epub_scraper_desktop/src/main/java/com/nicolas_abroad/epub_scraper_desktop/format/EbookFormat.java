967658d21f961b3d4db99786ee3db1ca3e587983
NicolasAbroad <40135543+NicolasAbroad@users.noreply.github.com>
2019-06-03 22:34:53 +0900

    Added format interfaces

---
+++ epub_scraper_desktop/src/main/java/com/nicolas_abroad/epub_scraper_desktop/format/EbookFormat.java
@@ -0,0 +1,6 @@
+package com.nicolas_abroad.epub_scraper_desktop.format;
+
+
+public interface EbookFormat {
+
+}
---
+++ epub_scraper_desktop/src/main/java/com/nicolas_abroad/epub_scraper_desktop/format/EpubFormat.java
@@ -0,0 +1,9 @@
+package com.nicolas_abroad.epub_scraper_desktop.format;
+
+/**
+ * Epub format.
+ * @author Nicolas
+ */
+public class EpubFormat implements EbookFormat {
+
+}
